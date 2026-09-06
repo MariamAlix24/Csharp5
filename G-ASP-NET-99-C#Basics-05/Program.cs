0c0d5ba71801e811c1e2705ff90dc374c7fbdd43
MariamAlix24 <alimariem59@gmail.com>
2026-09-06 13:53:17 +0300

    Question1

--- G-ASP-NET-99-C#Basics-05/Program.cs
+++ G-ASP-NET-99-C#Basics-05/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace G_ASP_NET_99_C_Basics_05
 {
+    #region Question1
+    class Book
+    {
+        private string password= "secret";
+    }
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Question1
+            Book book = new Book();
+            Console.WriteLine(book.password); // This will cause a compilation error because 'password' is private
+
         }
     }
 }
